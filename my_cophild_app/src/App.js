c42dd7d16716e299e8a5c8cb22eaaf71c7c20554
sourceboy <seunakanni417@gmail.com>
2024-07-17 07:19:33 -0500

    second commit

---
+++ my_cophild_app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Welcome () {
+  return(
+<div>
+    <h2>Welcome</h2>
+    <p>This is the begining of React course</p>
+    <div>
+      <ul>
+        <li>AN IT SPECIALIST</li>
+        <li>FULLSTACK DEVELOPER</li>
+        <li>AMAZON WEB SERVICES</li>
+      </ul>
+    </div>
+</div>
+
+  )
+}
+
+export default Welcome;
